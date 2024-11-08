f36dc6c7b69943ebe1934654f32f9af8997b4e91
trackfit-web <trackfit9@gmail.com>
2024-11-08 09:49:34 +0530

    Added bot

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./contexts/authContext/index.jsx";
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+
+// Initialize Kommunicate chat widget
+Kommunicate.init("3c33e2400ca58d7b37bbbf6b73cd95ade", {
+  automaticChatOpenOnNavigation: true,
+  popupWidget: true,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
